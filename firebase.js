f37ca2917413480b99d53af86311945b8cc45d00
jackoyatlas <jackoyatlas22@gmail.com>
2026-01-31 10:23:21 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,14 +3,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth
 import { getFirestore, enableIndexedDbPersistence } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCa4TGFuTu8u1HV06DDwfjtq35zWkGPDwM",
-    authDomain: "playhousetimer.firebaseapp.com",
-    projectId: "playhousetimer",
-    storageBucket: "playhousetimer.firebasestorage.app",
-    messagingSenderId: "850267989143",
-    appId: "1:850267989143:web:be3a03a2af6d1d0e66f5d2"
-  };
+  apiKey: "AIzaSyDSqhZyTKeOuNmu3DH7-dgv05JpqkifVS4",
+  authDomain: "playhgousetimer.firebaseapp.com",
+  projectId: "playhgousetimer",
+  storageBucket: "playhgousetimer.firebasestorage.app",
+  messagingSenderId: "887165264923",
+  appId: "1:887165264923:web:2fb7f546bb0cd640ff44be"
+};
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
